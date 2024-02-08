26ee062e1867635d8184618a5d3337df26e00376
Tkiliay <sackclouths@outlook.com>
2024-02-08 22:05:07 +0800

    clone miui camera
    Change-Id: I6f01ce4ced032fdf8736b2e0a79d33ff67fe8e0d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
+
+git clone -b uvite-leica https://gitlab.com/Tkiliay/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera
