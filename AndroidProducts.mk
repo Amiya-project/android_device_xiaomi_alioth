12dab34f1aa90c25e62b37d89ca4bda074f94620
Tkiliay <sackclouths@outlook.com>
2024-02-11 15:52:41 +0800

    alioth: Hi! Pixelstar.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/pixelstar_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    pixelstar_alioth-user \
+    pixelstar_alioth-userdebug \
+    pixelstar_alioth-eng
