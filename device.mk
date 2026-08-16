fbf9baa1c8d41a437049e9877b5e03fbbebdf947
Tkiliay <sackclouths@gmail.com>
2026-08-16 15:11:19 +0800

    alioth: Switch to stock color modes
    Change-Id: I35aea44d5ab5c456b3c0461576044ff069eb62a7

--- device.mk
+++ device.mk
@@ -44,6 +44,7 @@ $(call soong_config_set,xiaomi_kona,variant_lib,//$(LOCAL_PATH):libvariant_xiaom
 PRODUCT_PACKAGES += \
     ApertureOverlayDevice \
     FrameworkResOverlayDevice \
+    SettingsOverlayDevice \
     CustomSystemUIOverlayDevice \
     SystemUIOverlayDevice
 
